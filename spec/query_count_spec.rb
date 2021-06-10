6c8ad3235369cb18da9d01747c6b157e46629476
Dmitriy Tarasov <info@rubysamurai.com>
2021-06-10 14:54:46 +0300

    test: improve query counter spec

--- spec/query_count_spec.rb
+++ spec/query_count_spec.rb
@@ -6,8 +6,11 @@ RSpec.describe QueryCount do
     end
 
     it 'counts number of sql queries' do
-      7.times { User.last }
-      expect(QueryCount::Counter.counter).to eq 7
+      (1..100).each do |n|
+        QueryCount::Counter.reset_counter
+        n.times { User.last }
+        expect(QueryCount::Counter.counter).to eq n
+      end
     end
   end
 end
